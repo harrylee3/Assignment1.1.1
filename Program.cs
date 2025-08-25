7de0625d80aba3856daa7fb23408653e270995e1
harry <harry@HARRY-BARNES-PC>
2025-08-25 15:02:56 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string name = "Harry Barnes";
+string address = "71 Perry Lane, Dayton, TX 77535";
+int age = 37;
+
+Console.WriteLine($"User's name is: {name}.");
+Console.WriteLine($"User's age is: {age}.");
+Console.WriteLine($"User's address is: {address}.");
